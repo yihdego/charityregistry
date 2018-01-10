d91c4c134dfed51671b22e613df4c5e3a4c3adbf
yihdego <yihdego@msn.com>
2018-01-10 11:01:26 -0800

    added controller and rspec files for controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'registry/index'
+
+  get 'registry/create'
+
+  get 'registry/show'
+
+  get 'registry/edit'
+
+  get 'registry/update'
+
+  get 'registry/delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
